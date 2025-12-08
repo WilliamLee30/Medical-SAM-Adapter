409c622a90751fe95f7dbccf6d341ebca17e16a3
williamvvli <williamvvli@tencent.com>
2025-12-08 20:52:09 +0800

    env update

---
+++ readme.md
@@ -0,0 +1,5 @@
+# 1.环境配置
+由于原始的torch版本太旧，不支持H20的显卡，因此需要更新torch的版本，根据jiakuihu的建议，选取2.4版本的torch
+``` shell
+    pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
+```
---
+++ scripts/train_2D.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+exp_name="test_isic"
+sam_ckpt_path="/mnt/geminisgceph1/geminicephfs/mmsearch-luban-universal/group_semantic_video/user_williamvvli/pre_experiment/mitochondria_segmentation/model/sam_vit_b_01ec64.pth"
+data_path="/mnt/geminisgceph1/geminicephfs/mmsearch-luban-universal/group_semantic_video/user_williamvvli/pre_experiment/mitochondria_segmentation/data/ISIC"
+
+cd /mnt/geminisgceph1/geminicephfs/mmsearch-luban-universal/group_semantic_video/user_williamvvli/pre_experiment/mitochondria_segmentation/code/Medical-SAM-Adapter
+
+python -u train.py \
+    -net sam \
+    -mod sam_adpt \
+    -exp_name $exp_name \
+    -sam_ckpt $sam_ckpt_path \
+    -image_size 1024 \
+    -b 2 \
+    -dataset isic \
+    -vis 1 \
+    -data_path $data_path
+
+# nohup bash /mnt/geminisgceph1/geminicephfs/mmsearch-luban-universal/group_semantic_video/user_williamvvli/pre_experiment/mitochondria_segmentation/code/Medical-SAM-Adapter/scripts/train_2D.sh > /mnt/geminisgceph1/geminicephfs/mmsearch-luban-universal/group_semantic_video/user_williamvvli/pre_experiment/mitochondria_segmentation/experiment_results/logs/test_isic.log 2>&1 &
